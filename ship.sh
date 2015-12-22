6ac08214898dd3aaa5a90f9666d680859e268eb2
Eduardo Hayashi <eduardo.hayashi@climatempo.com.br>
2015-12-22 17:24:33 -0200

    Shipping with rebase to a specific branch
    - using rebase instead of merge

--- ship.sh
+++ ship.sh
@@ -1,6 +1,29 @@
-#!/bin/sh -x
-CURRENT=`git branch | grep "*" | awk '{print $2}'`
-git checkout master
-git merge ${CURRENT} 
-git commit -a -v
-git push origin master
+#!/bin/sh 
+##
+# author: Eduardo Hayashi
+# https://github.com/eduardohayashi/hack-sink-ship
+##
+
+getHelper()
+{
+   echo SHIP to \[BRANCH\] from current branch.
+   echo Syntax: ship \[target branch\]
+}
+
+CURRENT=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+
+if [ -z $1 ]; then
+    echo 'ERROR: Needed target branch to ship.\n'
+    getHelper
+    exit
+else
+    BRANCH=$1
+fi
+
+echo SHIPping to branch \"$BRANCH\"
+
+git checkout $BRANCH
+git rebase ${CURRENT}
+git push origin $BRANCH
+git checkout ${CURRENT}
