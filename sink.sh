856552467a2ecd5c6d9909a85170a2acabc477c5
Eduardo Hayashi <eduardo.hayashi@climatempo.com.br>
2015-12-22 17:24:33 -0200

    Sinking from a specific branch

--- sink.sh
+++ sink.sh
@@ -1,6 +1,29 @@
-#!/bin/sh -x
-CURRENT=`git branch | grep "*" | awk '{print $2}'`
-git checkout master
-git pull origin master
+#!/bin/sh
+##
+# author: Eduardo Hayashi
+# https://github.com/eduardohayashi/hack-sink-ship
+##
+
+##CONFIG
+DEVELOPBRANCH=develop
+
+CURRENT=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+getHelper()
+{
+   echo SINK branch \[BRANCH\] with current.
+   echo Syntax: sink \[source\]
+}
+
+if [ -z $1 ]; then
+    read -p "SINK with branch (default \"$DEVELOPBRANCH\"): " PROMPT
+    BRANCH=$(PROMPT:-$DEVELOPBRANCH)
+else
+    BRANCH=$1
+fi
+
+echo SINKing from branch \"$BRANCH\"
+git checkout $BRANCH
+git pull origin $BRANCH
 git checkout ${CURRENT}
-git rebase master ${CURRENT}
+git rebase $BRANCH
